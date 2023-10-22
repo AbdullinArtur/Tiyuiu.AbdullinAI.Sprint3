c3e3f16a482f35112d0d08b96eb9f25c664faff8
Tyuiu.AbdullinAI@outlook.com <holchik919@gmail.com>
2023-10-22 21:47:01 +0500

    Task2

---
+++ Tyuiu.AbdullinAI.Sprint3.Task2.V24.Test/DataServiceTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.AbdullinAI.Sprint3.Task2.V24.Lib;
+
+namespace Tyuiu.AbdullinAI.Sprint3.Task2.V24.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetSumSeries()
+        {
+            DataService ds = new DataService();
+
+            double value = 0.25;
+            int x = 1;
+            int y = 8;
+            double res = ds.GetSumSeries(value, x ,y);
+            double wait = 55217.446;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.AbdullinAI.Sprint3.Task2.V24/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.AbdullinAI.Sprint3.Task2.V24.Lib;
+
+namespace Tyuiu.AbdullinAI.Sprint3.Task2.V24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #3 | Выполнил: Абудллин А. И.  | АСОиУБ-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема:Оператор цикла do-while                                            *");
+            Console.WriteLine("* Задание #2                                                              *");
+            Console.WriteLine("* Вариант #24                                                             *");
+            Console.WriteLine("* Выполнил: Абдуллин А. И. | АСОиУБ-23-1                                  *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу используя цикл do...while                            *");
+            Console.WriteLine("* которая вычисляет произведение ряда по формуле, при a=0,25              *");
+            Console.WriteLine("*                                                                         *");
+            
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            double a = 0.25;
+            int start = 1;
+            int stop = 8;
+
+            Console.WriteLine("Значение a = " + a);
+            Console.WriteLine($"Старт шага  = {start}");
+            Console.WriteLine("Конец шага  = {0}", stop);
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            double res = ds.GetSumSeries(a, start, stop);
+            Console.WriteLine($"Результат: {res}");
+            Console.ReadKey();
+        }
+    }
+}
