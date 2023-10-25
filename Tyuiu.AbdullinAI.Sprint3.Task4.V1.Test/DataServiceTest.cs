0ac5defb931b30bb48ba0a7ded3b478ef44e5504
Tyuiu.AbdullinAI@outlook.com <holchik919@gmail.com>
2023-10-25 19:09:46 +0500

    Task4

---
+++ Tyuiu.AbdullinAI.Sprint3.Task4.V1.Test/DataServiceTest.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.AbdullinAI.Sprint3.Task4.V1.Lib;
+
+namespace Tyuiu.AbdullinAI.Sprint3.Task4.V1.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalculate()
+        {
+            DataService ds = new DataService();
+
+            int start = -5;
+            int stop = 5;
+
+            double res = ds.Calculate(start, stop);
+            double wait = 0.962;
+
+            Assert.AreEqual(wait, res);
+
+        }
+    }
+}
---
+++ Tyuiu.AbdullinAI.Sprint3.Task4.V1/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.AbdullinAI.Sprint3.Task4.V1.Lib;
+
+namespace Tyuiu.AbdullinAI.Sprint3.Task4.V1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #3 | Выполнил: Абудллин А. И.  | АСОиУБ-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: Использование операторов continue и break в циклах.               *");
+            Console.WriteLine("* Задание #4                                                              *");
+            Console.WriteLine("* Вариант #1                                                              *");
+            Console.WriteLine("* Выполнил: Абдуллин А. И. | АСОиУБ-23-1                                  *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* На отрезке, где x принимает значения от -5 до 5,                        *");
+            Console.WriteLine("* вычислить значение функции y=sin(x)/x                                   *");
+            Console.WriteLine("* При х = 0 прервать цикл. Полученные значения суммировать.               *");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int start = -5;
+            int stop = 5;
+
+            Console.WriteLine($"Старт шага = {start}");
+            Console.WriteLine($"Конец шага = {stop}");
+
+            
+
+
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            double res = ds.Calculate(start, stop);
+
+            Console.WriteLine($"Сумма ряда равна = {res}");
+
+            Console.ReadKey();
+
+        }
+    }
+}
